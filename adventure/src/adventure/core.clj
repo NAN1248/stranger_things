10bdf3861a5f3ff84bdc1175df046e94852dcc65
Tori Colthurst <vrc2@illinois.edu>
2017-12-17 20:46:43 -0600

    added party functionality

--- adventure/src/adventure/core.clj
+++ adventure/src/adventure/core.clj
@@ -11,39 +11,54 @@
     save him before it's too late! "
               :title "in your basement"
               :dir {:south :mirkwood, :upstairs :bedroom}
-              :contents #{:potato, :book}}
+              :people #{}
+              :contents #{}}
    :mirkwood {:desc "Mirkwood is a road that runs past the nearby forest. You see Will's bike lying on the side of the
 road, abandoned. "
               :title "on Mirkwood"
               :dir {:north :basement, :east :forest, :west :house}
-              :contents #{}}
+              :people #{}
+              :contents #{:bike}}
    :forest {:desc "The forest is a dangerous place at night. You, Dustin, and Lucas travel through the forest in
    hopes of finding Will. It begins to storm and Lucas is getting nervous. You turn your flashlight towards a noise
    you hear in the bushes... You've found Eleven! "
               :title "in the forest"
               :dir {:west :mirkwood}
+              :people #{:eleven}
               :contents #{}}
-   :house {:desc "The Byer's House is gloomy without Will there."
+   :house {:desc "The Byer's House is dark and empty."
               :title "in the Byer's House"
               :dir {:east :mirkwood}
-              :contents #{}}
+              :people #{}
+              :contents #{:bat}}
    :bedroom {:desc "The room is covered in striped wallpaper and pictures of Nancy's friends. On top of Nancy's bed
 you find Steve Harrington fixing his hair. "
               :title "in Nancy's Bedroom"
               :dir {:downstairs :basement, :south :mirkwood}
+              :people #{}
               :contents #{}}})
-
 
 (def adventurer
   {:location :basement
    :inventory #{}
+   :party #{:Dustin, :Lucas}
    :tick 0
    :seen #{}})
 
 (defn status [player]
   (let [location (player :location)]
     (println (str "You are " (-> the-map location :title) ". "))
-    (println (str "In your bag: " (player :inventory) " --> "))
+    (println)
+    (when-not ((player :seen) location)
+      (print (-> the-map location :desc)))
+    (update-in player [:seen] #(conj % location))))
+
+(defn update [player]
+  (let [location (player :location)]
+    (println (str "You are " (-> the-map location :title) ". "))
+    (println (str "In your bag: " (player :inventory) " "))
+    (println (str "Your party consists of: " (player :party) " "))
+    (println)
     (when-not ((player :seen) location)
       (print (-> the-map location :desc)))
     (update-in player [:seen] #(conj % location))))
@@ -67,6 +82,14 @@ you find Steve Harrington fixing his hair. "
       (do (println "You can't pick that up.") player)
       (update-in player [:inventory] #(conj % item)))))
 
+(defn addparty [people player]
+  (let [location (player :location)
+              person (->> the-map location :people people)]
+    (if (nil? person)
+      (do (println "There's no one here.") player)
+      (update-in player [:party] #(conj % person)))))
+
+
 
 (defn tock [player]
   (update-in player [:tick] inc))
@@ -80,8 +103,10 @@ you find Steve Harrington fixing his hair. "
          [:west] (go :west player)
          [:upstairs] (go :upstairs player)
          [:downstairs] (go :downstairs player)
-         [:pickup_potato] (pickup :potato player)
-         [:pickup_book] (pickup :book player)
+         ;[:pickup_potato] (pickup :potato player)
+         ;[:pickup_book] (pickup :book player)
+         [:friend_eleven] (addparty :eleven player)
+         [:status] (update player)
 
          _ (do (println "I don't understand you.")
                player)))
