a9476bfd543c972dd87ef1174cf1f7c2880e8b2f
Nathan Narasimhan <nanaras2@illinois.edu>
2017-12-18 00:02:36 -0600

    added more rooms

--- adventure/src/adventure/core.clj
+++ adventure/src/adventure/core.clj
@@ -26,20 +26,37 @@ Type pickup_bike to grab Will's bike."
 hopes of finding Will. It begins to storm and Lucas is getting nervous. You turn your flashlight towards a noise
 you hear in the bushes... You've found Eleven!"
               :title "in the forest"
-              :dir {:west :mirkwood}
+              :dir {:west :mirkwood, :south :store, :east :cliff}
               :people #{:eleven}
+              :help "Type 'friend_eleven' to add eleven to your party. \nType 'west' to go to Mirkwood."
               :contents #{}}
    :house {:desc "The Byer's House is dark and empty. You hear sounds coming from Will's room. The lights start to flicker, and you
 recognize the song 'Should I Stay or Should I Go'..."
               :title "in the Byer's House"
               :dir {:east :mirkwood}
               :people #{}
-              :contents #{:bat}}
+              :help "Type 'east' to go to Mirkwood."
+              :contents #{:bat}}   
+    ; ASSUME WE HAVE ELEVEN. DO NOT LET THEM HERE IF THEY DO NOT          
+   :store {:desc "The grocery store is the place for food. We need to buy Eleven some food because she is hungry."
+              :title "The grocery store"
+              :dir {:north :forest}
+              :people #{}
+              :help "Type 'north' to go to forest."
+              :contents #{:eggo_waffles, :frozen_pizza}}
+   :cliff {:desc "You take a good look at the beatiful waterfall, and you realize this is one of the best views you have seen in your life. \n
+                  Then you realize there is a massive cliff. You need to see if you can trust Eleven, so you must test her strength."
+              :title "The Cliff"
+              :dir {:west :forest}
+              :people #{}
+              :help "Type 'west' to go to forest.\n Type 'pickup_key' to pickup the key. \nType 'jump' to jump off the cliff"
+              :contents #{:key}}
    :bedroom {:desc "The room is covered in striped wallpaper and pictures of Nancy's friends. On top of Nancy's bed
 you find Steve Harrington fixing his hair. "
               :title "in Nancy's Bedroom"
               :dir {:downstairs :basement, :south :mirkwood}
               :people #{}
+              :help "Type 'downstairs' to go to basement. \nType 'south' to go to Mirkwood."
               :contents #{}}})
 
 (def adventurer
